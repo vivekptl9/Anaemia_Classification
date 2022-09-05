ac561c33106591548e669e0c1b3b83b05697667c
Vivek Patel <vivekptl9@gmail.com>
2022-09-05 11:47:49 +0200

    done CNN-random forest model

---
+++ CNN-RF.py
@@ -0,0 +1,9 @@
+import numpy as np 
+import matplotlib.pyplot as plt
+import glob
+import cv2
+import tensorflow as tf
+from keras.models import Model, Sequential
+from keras.layers import Dense, Flatten, Conv2D, MaxPooling2D
+import os
+import seaborn as sns
