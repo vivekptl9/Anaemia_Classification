2eab190dc34e6e32273956eedbdad191a6d43636
Vivek Patel <vivekptl9@gmail.com>
2022-09-05 12:44:05 +0200

    Made a Strimlit branch for phillip to deploy

--- CNN-RF.py
+++ CNN-RF.py
@@ -7,3 +7,13 @@ from keras.models import Model, Sequential
 from keras.layers import Dense, Flatten, Conv2D, MaxPooling2D
 import os
 import seaborn as sns
+
+#Loadind the data into a numpy array:
+
+def Data_loading():
+    with open('/home/lepton/Downloads/Anaemia/Anaemia/pictures_array.npy', 'rb') as f:
+        pics_all = np.load(f)
+    with open('/home/lepton/Downloads/Anaemia/Anaemia/labels_array.npy', 'rb') as f:
+        labels_all = np.load(f)
+    print(pics_all[0].shape)
+    return pics_all,labels_all
