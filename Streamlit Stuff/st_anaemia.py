79aafc030fd0817fdc4e5251b58871b8bfc3b218
Philip Steffen <philipsteffenprivat@gmail.com>
2022-09-07 11:33:49 +0200

    reordered stuff

--- Streamlit Stuff/st_anaemia.py
+++ Streamlit Stuff/st_anaemia.py
@@ -2,7 +2,10 @@ import streamlit as st
 import matplotlib.pyplot as plt
 from PIL import Image
 import numpy as np
-import Anaemia_Classification
+#import Anaemia_Classification
+from dataframe_creation.resizing import resize_image
+from google.cloud import storage
+import requests
 
 #---------------------------------#
 # Load both models
@@ -15,11 +18,17 @@ import Anaemia_Classification
 ## Page expands to full width
 st.set_page_config(layout="wide")
 
+
+
+
 #---------------------------------#
 #Header
 st.header("""
          # Detection of *Sickle* *Cell* *Disease* (Anaemia) from blood smears!
          # """)
+
+
+
 
 #---------------------------------#
 #Expandable About bar
@@ -34,20 +43,48 @@ Made by
 and [Philip](https://www.linkedin.com/in/philip-steffen-71b5b823a/).
 """)
 
+
+
 #Picture of Blood smear
 #image = Image.open('image.jpg')
 #st.image(image, width = 200)
 
 #st.subheader('Please upload an image to do a prediction on!')
 
+
+
+
 #Loading test picture
-with open('notebooks/pictures_array.npy', 'rb') as f:
-    pictures = np.load(f)
+with open('one_img.npy', 'rb') as f:
+    picture = np.load(f)
+
+
+
+
+
 
 #---------------------------------#
 # Image Upload
 st.subheader('Please upload your image for analysis below:')
-uploaded_image = st.file_uploader("")
+uploaded_image = st.file_uploader("",type=["png","jpg","jpeg","tiff"])
+
+
+
+
+#---------------------------------#
+# Resize Image
+if uploaded_image != None:
+    img_path = "resized_image.jpg"
+    resized_img = resize_image(uploaded_image)
+    im = (resized_img * 255).astype(np.uint8)
+    im = Image.fromarray(im)
+    im.save(img_path)
+#---------------------------------#
+# Uploading resized img to google cloud
+    client = storage.Client()
+    bucket = client.bucket('anaemia')
+    blob = bucket.blob('img_website_upload.jpg')
+    blob.upload_from_filename(img_path)
 
 #---------------------------------#
 # Splitting Website into 2 columns (same size)
@@ -58,24 +95,24 @@ col1.subheader('Uploaded Image')
 
 plt.figure(figsize=(5,5), frameon=False)
 if uploaded_image == None:
-    plt.imshow(pictures[0], cmap='Reds')
+    plt.imshow(picture, cmap='Reds')
     col1.pyplot(plt)
+    col1.text('(Shows sample images as long as nothing is uploaded)')
 else:
-    plt.imshow(uploaded_image, cmap='Reds')
-    col1.pyplot(plt)
-
+    col1.image(im, width = 300)
 # call function to rescale picture and save as uploaded_rescaled
 # call function to get image with outlines and sickle cells with rectangles if there (uploaded_outlines)
 
+
+
 col2.subheader('Computer generated Image from Upload')
-st.text('(Shows sample images as long as nothing is uploaded)')
+
 plt.figure(figsize=(5,5), frameon=False)
 if uploaded_image == None:
-    plt.imshow(pictures[0], cmap='Reds')
+    plt.imshow(picture, cmap='Reds')
     col2.pyplot(plt)
 else:
-    plt.imshow(uploaded_image, cmap='Reds')
-    col2.pyplot(plt)
+    col2.image(im, width = 300)
 
 
 
@@ -93,8 +130,11 @@ else:
 # proba_final = get_final_proba(proba_cnn, proba_r_f) # Voting Classifier to get final Probability
 
 proba_final = 99
-proba_cnn = 95
+proba_cnn = 99
 proba_rf = 92
 
-st.header(f'The probabilty for Anaemia in the given sample is: {proba_final} %')
+url = 'http://127.0.0.1:8000/predict'
+proba_final = requests.get(url).json()['response']
+
+st.header(f'The probability for Anaemia in the given sample is: {proba_final} %')
 st.text(f'Our DeepLearning model predicts {proba_cnn}% probability and our Machine Learning model predicts {proba_rf}%')
